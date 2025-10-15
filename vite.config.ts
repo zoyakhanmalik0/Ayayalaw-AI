d46419c411c47aacfdcf5e89c7c81a50e380a02f
zoyakhanmalik0 <zoyakhanmalik0@gmail.com>
2025-10-16 04:45:59 +0500

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',              // ✅ Important for Vercel routing
+  base: '/',
   plugins: [react()],
   build: {
-    outDir: 'dist',       // ✅ Vercel expects this folder
-  }
+    outDir: 'dist',
+  },
 })
